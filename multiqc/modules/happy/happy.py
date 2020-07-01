3c42087abc0b3dd308b9687e5d315306f50f4e38
sophie22 <sophie22@users.noreply.github.com>
2020-07-01 12:00:19 +0100

    h2

--- multiqc/modules/happy/happy.py
+++ multiqc/modules/happy/happy.py
@@ -70,7 +70,7 @@ class MultiqcModule(BaseMultiqcModule):
 
                 Ideally, precision, recall and F1 Score should all be as close to 1 as possible.
             ''',
-            plot = table.plot(self.happy_indel_data, self.gen_headers(), indel_config)
+            plot = table.plot(self.happy_indel_data)
         )
 
         snp_config = {
@@ -91,7 +91,7 @@ class MultiqcModule(BaseMultiqcModule):
 
                 Ideally, precision, recall and F1 Score should all be as close to 1 as possible.
             ''',
-            plot = table.plot(self.happy_snp_data, self.gen_headers(), snp_config)
+            plot = table.plot(self.happy_snp_data, self.gen_headers())
         )
 
     def parse_file(self, f):
